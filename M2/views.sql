43bbb79eee96b9b26bf025f0ac24496bb19e841d
Arévalo 3 <martaarevaloprieto@gmail.com>
2024-01-08 20:10:09 +0100

    actualización

--- M2/views.sql
+++ M2/views.sql
@@ -87,3 +87,109 @@ select p.player_id as 'PlayerID',
 
 from players p join game g on p.player_id = g.player_id
 			   join enemies e on g.game_id = e.game_id;
+               
+
+-- VISTES DE CONSULTES A LA BD
+
+-- 1) usuaris que han jugat
+
+CREATE OR REPLACE VIEW query_1_usuaris as
+select p.user_name as 'NomUsuari',
+	   MAX(g.date_started) as 'UltimaPartida'
+
+from players p join game g on p.player_id = g.player_id
+group by p.player_id,p.user_name
+order by p.user_name;
+
+-- 2) quantitat de partides jugades per usuari
+
+CREATE OR REPLACE VIEW query_2_partides as
+select p.user_name as 'NomUsuari', 
+	   COUNT(g.game_id) as 'PartidesJugades'
+
+from players p join game g on p.player_id = g.player_id
+group by p.player_id, p.user_name
+order by p.user_name;
+
+-- 3) Armes usades per cada usuari i dades de la partida on n'ha gastat més: Consulta a través de la vista --- mal ya que da solo la sum de esa partida
+
+CREATE OR REPLACE VIEW query_3_armes as
+SELECT
+    NomUsuari,
+    NomArma,
+    QuantitatTotalObtenida,
+    DataPartida
+FROM (
+    SELECT
+        NomUsuari,
+        NomArma,
+        QuantitatTotalObtenida,
+        DataPartida,
+        RANK() OVER (PARTITION BY NomUsuari, NomArma ORDER BY QuantitatTotalObtenida DESC) AS RankPartida
+    FROM (
+        SELECT
+            NomUsuari,
+            NomArma,
+            SUM(QuantitatObtenida) AS QuantitatTotalObtenida,
+            MAX(DataPartida) AS DataPartida
+        FROM
+            v_armas_partidas
+        GROUP BY
+            NomUsuari, NomArma
+    ) AS SubconsultaTotal
+) AS SubconsultaRankeada
+WHERE
+    RankPartida = 1;
+    
+-- 4) Menjar consumit per cada usuari i dades de la partida on n'ha consumit més: consulta a través de la vista
+
+CREATE OR REPLACE VIEW query_4_menjar as
+SELECT
+    NomUsuari,
+    NomMenjar,
+    QuantitatTotalObtenida,
+    DataPartida
+FROM (
+    SELECT
+        NomUsuari,
+        NomMenjar,
+        QuantitatTotalObtenida,
+        DataPartida,
+        RANK() OVER (PARTITION BY NomUsuari, NomMenjar ORDER BY QuantitatTotalObtenida DESC) AS RankPartida
+    FROM (
+        SELECT
+            NomUsuari,
+            NomMenjar,
+            SUM(QuantitatObtenida) AS QuantitatTotalObtenida,
+            MAX(DataPartida) AS DataPartida
+        FROM
+            v_comidas_partidas
+        GROUP BY
+            NomUsuari, NomMenjar
+    ) AS SubconsultaTotal
+) AS SubconsultaRankeada
+WHERE
+    RankPartida = 1;
+    
+-- 5) Estadística de "blood moons"
+
+      -- mitjana blood moons
+      
+CREATE OR REPLACE VIEW query_5_mitjanabm as
+select avg(blood_moon_appearences) as 'MediaBloodMoons'
+from game;
+
+      -- dades de la partida on han aparegut més
+
+CREATE OR REPLACE VIEW query_5_mesbm as
+SELECT
+    g.date_started AS DataPartida,
+    p.user_name AS NomUsuari,
+    g.blood_moon_appearences AS QuantitatBloodMoons
+FROM
+    game g
+JOIN
+    players p ON g.player_id = p.player_id
+ORDER BY
+    g.blood_moon_appearences DESC
+LIMIT 1;
